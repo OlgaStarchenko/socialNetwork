9cfed3b4a38d607bcd8127bc875b6650d4791a75
OlgaStarchenko <o9915530@gmail.com>
2026-08-29 20:42:25 +1000

    A skeleton has been added to PersonPage. Conditional rendering of the skeleton during data loading is implemented using the `isLoading` state obtained from the `useGetUserByIdQuery` and `useGetUserByIdPostsQuery` hooks.

--- src/pages/PersonPage.jsx
+++ src/pages/PersonPage.jsx
@@ -8,6 +8,7 @@ import {
   Flex,
   Heading,
   Text,
+  Skeleton,
 } from "@radix-ui/themes";
 import PostList from "../components/PostList";
 import { useGetUserByIdQuery, useGetUserByIdPostsQuery } from "../api/usersApi";
@@ -15,18 +16,21 @@ import { useGetUserByIdQuery, useGetUserByIdPostsQuery } from "../api/usersApi";
 export default function PersonPage() {
   const { id } = useParams();
 
-  const { data } = useGetUserByIdQuery(id, {
+  const { data, isLoading: isLoadingUser } = useGetUserByIdQuery(id, {
     skip: !id,
   });
   const user = data ? data : {};
 
-  const { data: dataPosts } = useGetUserByIdPostsQuery(id, {
-    skip: !id,
-  });
+  const { data: dataPosts, isLoading: isLoadingPosts } =
+    useGetUserByIdPostsQuery(id, {
+      skip: !id,
+    });
 
   const posts = dataPosts ? dataPosts.posts : [];
 
-  console.log(dataPosts);
+  const skeletonsPosts = [...new Array(3)].map((_, index) => (
+    <Skeleton key={index} width="100%" height="250px" />
+  ));
 
   return (
     <>
@@ -37,53 +41,105 @@ export default function PersonPage() {
           <Flex style={{ justifyContent: "space-around" }}>
             <Card>
               <Flex gap="3" align="center">
-                <Avatar size="3" src={user.image} radius="full" fallback="T" />
+                {isLoadingUser ? (
+                  <Skeleton height={"40px"} width={"40px"} />
+                ) : (
+                  <Avatar
+                    size="3"
+                    src={user.image}
+                    radius="full"
+                    fallback="T"
+                  />
+                )}
+
                 <Box>
+                  <Flex direction={"column"} gap={"1"}>
+                    {isLoadingUser ? (
+                      <Skeleton>
+                        <Text>Nicholas Bailey</Text>
+                      </Skeleton>
+                    ) : (
+                      <Text
+                        as="div"
+                        size="2"
+                        weight="bold"
+                        style={{ color: "initial" }}
+                      >
+                        {user.firstName} {user.lastName}
+                      </Text>
+                    )}
+                    {isLoadingUser ? (
+                      <Skeleton>
+                        <Text>nicholasb</Text>
+                      </Skeleton>
+                    ) : (
+                      <Text as="div" size="2" color="gray">
+                        {user.username}
+                      </Text>
+                    )}
+                  </Flex>
+                </Box>
+              </Flex>
+            </Card>
+            <Card>
+              <Flex direction={"column"} gap={"1"}>
+                {isLoadingUser ? (
+                  <Skeleton>
+                    <Text>nicholas.bailey@x.dummyjson.com</Text>
+                  </Skeleton>
+                ) : (
                   <Text
                     as="div"
                     size="2"
                     weight="bold"
                     style={{ color: "initial" }}
                   >
-                    {user.firstName} {user.lastName}
+                    {user.email}
                   </Text>
+                )}
+                {isLoadingUser ? (
+                  <Skeleton>
+                    <Text>+49 212-856-4272</Text>
+                  </Skeleton>
+                ) : (
                   <Text as="div" size="2" color="gray">
-                    {user.username}
+                    {user.phone}
                   </Text>
-                </Box>
+                )}
               </Flex>
             </Card>
             <Card>
-              <Text
-                as="div"
-                size="2"
-                weight="bold"
-                style={{ color: "initial" }}
-              >
-                {user.email}
-              </Text>
-              <Text as="div" size="2" color="gray">
-                {user.phone}
-              </Text>
-            </Card>
-            <Card>
-              <Text
-                as="div"
-                size="2"
-                weight="bold"
-                style={{ color: "initial" }}
-              >
-                {user.birthDate}
-              </Text>
-              <Text as="div" size="2" color="gray">
-                {user.age} years old
-              </Text>
+              <Flex direction={"column"} gap={"1"}>
+                {isLoadingUser ? (
+                  <Skeleton>
+                    <Text>1989-6-14</Text>
+                  </Skeleton>
+                ) : (
+                  <Text
+                    as="div"
+                    size="2"
+                    weight="bold"
+                    style={{ color: "initial" }}
+                  >
+                    {user.birthDate}
+                  </Text>
+                )}
+                {isLoadingUser ? (
+                  <Skeleton>
+                    <Text>36 years old</Text>
+                  </Skeleton>
+                ) : (
+                  <Text as="div" size="2" color="gray">
+                    {user.age} years old
+                  </Text>
+                )}
+              </Flex>
             </Card>
           </Flex>
         </Card>
         <Heading my={"5"}>Person Posts</Heading>
-        <Flex direction="column">
-          <PostList posts={posts} />
+        <Flex direction="column" gap={"4"}>
+          {isLoadingPosts ? skeletonsPosts : <PostList posts={posts} />}
         </Flex>
       </Container>
     </>
